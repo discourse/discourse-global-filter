dbc76e88a3ae769d7c186a3e3f1faee8ac13c7a2
awesomerobot <kris.aubuchon@discourse.org>
2022-08-11 14:57:20 -0400

    UX: remove active class, tag name formatting
    Signed-off-by: awesomerobot <kris.aubuchon@discourse.org>

--- assets/javascripts/discourse/components/global-filter-item.js
+++ assets/javascripts/discourse/components/global-filter-item.js
@@ -1,9 +1,15 @@
 import Component from "@ember/component";
 import { action } from "@ember/object";
+import discourseComputed from "discourse-common/utils/decorators";
 import DiscourseURL from "discourse/lib/url";
 
 export default Component.extend({
   classNames: ["global-filter-item"],
+
+  @discourseComputed("filter")
+  spacedTag(filter) {
+    return filter.replace(/-|_/g, " ");
+  },
 
   @action
   selectFilter(tag) {
