8fb76a96897e12f1058b723c0af8ae0e0a21a065
Isaac Janzen <isaac.janzen@discourse.org>
2022-08-11 16:52:24 -0500

    Add filter items to composer

---
+++ assets/javascripts/discourse/components/global-filter-composer-container.js
@@ -0,0 +1,14 @@
+import Component from "@ember/component";
+import discourseComputed from "discourse-common/utils/decorators";
+
+export default Component.extend({
+  classNames: ["global-filter-composer-container"],
+
+  @discourseComputed("siteSettings.global_filters")
+  globalFilters(filters) {
+    if (!filters) {
+      return false;
+    }
+    return filters.split("|");
+  },
+});
