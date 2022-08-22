e7b30c25363382930c300faa244cee5c615d631b
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2022-08-22 08:58:35 -0500

    Hide GTF values from tag-chooser (#16)
    * Add custom tag chooser
    * add filter to composer

--- assets/javascripts/discourse/components/global-filter/composer-container.js
+++ assets/javascripts/discourse/components/global-filter/composer-container.js
@@ -4,6 +4,10 @@ import { inject as service } from "@ember/service";
 export default class GlobalFilterComposerContainer extends Component {
   @service siteSettings;
 
+  canDisplay =
+    this.args.composer.creatingTopic === true &&
+    !this.args.composer.creatingPrivateMessage;
+
   get globalFilters() {
     const filters = this.siteSettings.global_filters;
     if (!filters) {
