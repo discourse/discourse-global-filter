1867dd5d9e9e2e737d033655ac39bef617e6d18a
Penar Musaraj <pmusaraj@gmail.com>
2022-08-25 16:29:26 -0400

    FIX: Don't show filters bar in admin routes

--- assets/javascripts/discourse/components/global-filter-container.js
+++ assets/javascripts/discourse/components/global-filter-container.js
@@ -1,12 +1,14 @@
 import Component from "@ember/component";
 import discourseComputed from "discourse-common/utils/decorators";
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
   tagName: "",
+  router: service(),
 
-  @discourseComputed("siteSettings.global_filters")
-  globalFilters(filters) {
-    if (!filters) {
+  @discourseComputed("siteSettings.global_filters", "router.currentRouteName")
+  globalFilters(filters, routeName) {
+    if (!filters || routeName.startsWith("admin")) {
       return false;
     }
     return filters.split("|");
