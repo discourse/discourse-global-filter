db4ceede1282c957fd873aafe0dbf71ddbc1b7a7
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2022-08-28 20:32:47 -0500

    Add GFT to composer when creating topic from Categories (#22)

--- assets/javascripts/discourse/components/global-filter/composer-item.js
+++ assets/javascripts/discourse/components/global-filter/composer-item.js
@@ -1,9 +1,35 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
+import { makeArray } from "discourse-common/lib/helpers";
+import { inject as service } from "@ember/service";
 
 export default class GlobalFilterComposerItem extends Component {
-  checked = this.args.composer.tags?.includes(this.args.filter) ? true : false;
+  @service router;
+
+  checked;
   spacedTag = this.args.filter.replace(/-|_/g, " ");
+
+  constructor() {
+    super(...arguments);
+
+    const tagParam = this.router.currentRoute?.queryParams?.tag;
+    if (tagParam) {
+      const composer = this.args.composer;
+      if (composer.tags) {
+        if (composer.tags.includes(tagParam)) {
+          composer.tags.push(tagParam);
+        }
+      } else {
+        this.args.composer.set("tags", makeArray(tagParam));
+      }
+    }
+
+    this.checked = this.args.composer.tags?.includes(
+      this.args.filter || tagParam
+    )
+      ? true
+      : false;
+  }
 
   @action
   toggleTag() {
