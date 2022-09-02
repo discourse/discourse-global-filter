c19974d81e8a67de5f7260b68e8333e428fcddf0
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2022-09-02 13:51:54 -0500

    Update routing logic (#29)

--- assets/javascripts/discourse/initializers/global-filter-preference.js
+++ assets/javascripts/discourse/initializers/global-filter-preference.js
@@ -8,9 +8,9 @@ const ROUTES_TO_REDIRECT_ON = [
   "discovery.top",
   "discovery.unread",
   "discovery.category",
+  "discovery.categories",
   "tag.show",
   "tags.intersection",
-  "tags.showCategory",
 ];
 
 export default {
@@ -61,7 +61,9 @@ export default {
 
       if (ROUTES_TO_REDIRECT_ON.includes(routeName)) {
         const additionalTags = transition.to?.params?.additional_tags;
-        const tag = transition.to?.params?.tag_id;
+        const tag =
+          transition.to?.queryParams?.tag || transition.to?.params?.tag_id;
+
         let filterPref;
         let tagCombination;
 
@@ -71,16 +73,14 @@ export default {
 
         if (currentUser) {
           if (globalFilters.includes(tag)) {
-            this._setClientAndServerFilterPref(tag, currentUser).then(() => {
-              filterPref = this._setClientFilterPref(tag, currentUser);
-              this._redirectToFilterPref(
-                transition,
-                router,
-                filterPref,
-                true,
-                additionalTags || false
-              );
-            });
+            filterPref = this._setClientFilterPref(tag, currentUser);
+            this._redirectToFilterPref(
+              transition,
+              router,
+              filterPref,
+              true,
+              additionalTags || false
+            );
           } else {
             filterPref = currentUser.custom_fields.global_filter_preference;
             this._redirectToFilterPref(
@@ -131,8 +131,6 @@ export default {
       const queryParams = transition?.to?.queryParams;
       const categorySlug = transition.to?.params?.category_slug_path_with_id;
 
-      // if the route is tag.show but we won't be redirecting to a
-      // tag intersection or a category, just return
       if (
         transition.to?.name === "tag.show" &&
         !(additionalTags || categorySlug)
@@ -143,20 +141,17 @@ export default {
       if (!globalFilterPresent || transition.to?.name === "tag.show") {
         if (additionalTags) {
           url = `/tags/intersection/${filterPref}/${additionalTags}`;
+          router.transitionTo(url, null, { queryParams });
+        } else if (transition.to?.localName === "categories") {
+          transition.abort();
+          router.transitionTo(`/categories?tag=${filterPref}`);
+        } else if (transition.to?.name === "discovery.latest") {
+          router.transitionTo(`/tag/${filterPref}`, null, { queryParams });
         } else {
           const categoryURL = categorySlug ? `s/c/${categorySlug}` : "";
           url = `/tag${categoryURL}/${filterPref}`;
+          router.transitionTo(url, null, { queryParams });
         }
-
-        router.transitionTo(url, null, { queryParams });
-      } else if (
-        ROUTES_TO_REDIRECT_ON.includes(transition.intent?.targetName)
-      ) {
-        router.transitionTo(
-          `/tag/${filterPref}/l/${transition.to.localName}`,
-          null,
-          { queryParams }
-        );
       }
     });
   },
