ef9f154b19d2ed48fc92c284b0be57fe2c3d61f7
Isaac Janzen <isaac.janzen@discourse.org>
2022-09-19 10:11:09 -0500

    Fix included tags on topic creation

--- assets/javascripts/discourse/components/global-filter/composer-container.js
+++ assets/javascripts/discourse/components/global-filter/composer-container.js
@@ -1,9 +1,12 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
+import { makeArray } from "discourse-common/lib/helpers";
 
 export default class GlobalFilterComposerContainer extends Component {
   @service siteSettings;
+  @service router;
 
+  tagParam = this.router.currentRoute?.queryParams?.tag;
   canDisplay =
     this.args.composer.creatingTopic === true &&
     !this.args.composer.creatingPrivateMessage;
@@ -14,5 +17,20 @@ export default class GlobalFilterComposerContainer extends Component {
       return false;
     }
     return filters.split("|");
+  }
+
+  constructor() {
+    super(...arguments);
+
+    if (this.tagParam) {
+      const composer = this.args.composer;
+      if (composer.tags) {
+        if (composer.tags.includes(this.tagParam)) {
+          composer.tags.push(this.tagParam);
+        }
+      } else {
+        this.args.composer.set("tags", makeArray(this.tagParam));
+      }
+    }
   }
 }
--- assets/javascripts/discourse/components/global-filter/composer-item.js
+++ assets/javascripts/discourse/components/global-filter/composer-item.js
@@ -1,32 +1,15 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
-import { makeArray } from "discourse-common/lib/helpers";
-import { inject as service } from "@ember/service";
 
 export default class GlobalFilterComposerItem extends Component {
-  @service router;
-
-  checked;
   spacedTag = this.args.filter.replace(/-|_/g, " ");
+  checked;
 
   constructor() {
     super(...arguments);
 
-    const tagParam = this.router.currentRoute?.queryParams?.tag;
-
-    if (tagParam) {
-      const composer = this.args.composer;
-      if (composer.tags) {
-        if (composer.tags.includes(tagParam)) {
-          composer.tags.push(tagParam);
-        }
-      } else {
-        this.args.composer.set("tags", makeArray(tagParam));
-      }
-    }
-
     this.checked = this.args.composer.tags?.includes(
-      this.args.filter || tagParam
+      this.args.filter || this.args.tagParam
     )
       ? true
       : false;
