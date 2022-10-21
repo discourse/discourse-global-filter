514f7ebbea3366b56cbca2f47e1971f98ec608f0
Isaac Janzen <isaac.janzen@discourse.org>
2022-10-21 06:30:36 -0500

    site serializer

--- lib/site_serializer_extension.rb
+++ lib/site_serializer_extension.rb
@@ -4,9 +4,8 @@ module GlobalFilter::SiteSerializerExtension
 
   def categories
     filter_tag_category_ids = GlobalFilter::FilterTag.find_by(name: filter_tag)&.category_ids&.split("|")&.map(&:to_i)
-    return object.categories.map(&:to_h) if !filter_tag_category_ids
-
+    filter_tag_category_ids = Category.secured(scope).pluck(:id) if filter_tag_category_ids.empty?
     filtered_categories = object.categories.select {|c| filter_tag_category_ids.include?(c[:id])}
-    filtered_categories.map(&:to_h)
+    filtered_categories.empty? ? object.categories.map(&:to_h) : filtered_categories.map(&:to_h)
   end
 end
