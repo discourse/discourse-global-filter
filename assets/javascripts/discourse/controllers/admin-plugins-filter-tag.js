28cae7881d795abcca8451be0d46e5c96457aec2
Isaac Janzen <isaac.janzen@discourse.org>
2022-10-24 16:40:28 -0500

    Fix admin ui styles / category display

--- assets/javascripts/discourse/controllers/admin-plugins-filter-tag.js
+++ assets/javascripts/discourse/controllers/admin-plugins-filter-tag.js
@@ -6,24 +6,28 @@ import { popupAjaxError } from "discourse/lib/ajax-error";
 export default class extends Controller {
   @action
   setCategoryIdsForTag(filterTag, categories) {
-    let category_ids = [];
+    let categoryIds = [];
+    let categoriesAndParents = [];
 
+    categoriesAndParents.push(categories);
     categories.map((c) => {
-      category_ids.push(c.id);
+      categoryIds.push(c.id);
 
       // if category has parent, include parent
       if (c.parent_category_id) {
-        category_ids.push(c.parent_category_id);
+        categoryIds.push(c.parent_category_id);
+        categoriesAndParents.push(c.parentCategory);
 
         // check if category is subsubcategory
         if (c.parentCategory.parent_category_id) {
-          category_ids.push(c.parentCategory.parent_category_id);
+          categoryIds.push(c.parentCategory.parent_category_id);
+          categoriesAndParents.push(c.parentCategory.parentCategory);
         }
       }
     });
 
     const data = {
-      category_ids,
+      category_ids: categoryIds,
     };
 
     return ajax(
@@ -34,7 +38,9 @@ export default class extends Controller {
       }
     )
       .then(() => {
-        filterTag.set("categories", categories);
+        console.log(categoriesAndParents);
+        const uniqCategories = Array.from(new Set(categoriesAndParents.flat()));
+        filterTag.set("categories", uniqCategories);
       })
       .catch(popupAjaxError);
   }
