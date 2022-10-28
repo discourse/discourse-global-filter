fbae599b16c90632ebb97b6d9cc8ea5f2ab88590
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2022-10-28 13:41:50 -0500

    Filter categories for anonymous users (#54)
    * Filter categories for anon users
    * Add tests

---
+++ assets/javascripts/discourse/initializers/filter-category-list-for-anon.js
@@ -0,0 +1,34 @@
+import CategoryList from "discourse/models/category-list";
+import PreloadStore from "discourse/lib/preload-store";
+import { ajax } from "discourse/lib/ajax";
+
+export default {
+  name: "filter-category-list-for-anon",
+
+  initialize(container) {
+    const siteSettings = container.lookup("service:site-settings");
+    if (siteSettings.discourse_global_filter_enabled) {
+      CategoryList.reopenClass({
+        list(store) {
+          const tagParam = new URLSearchParams(window.location.search).get(
+            "tag"
+          );
+          // Since core makes an additional ajax call to /categories
+          // we need to override the list function to pass a tag parameter
+          // so that we serve filtered (by GFT) categories
+          const getCategories = () => ajax(`/categories.json?tag=${tagParam}`);
+          return PreloadStore.getAndRemove(
+            "categories_list",
+            getCategories
+          ).then((result) => {
+            return CategoryList.create({
+              categories: this.categoriesFrom(store, result),
+              can_create_category: result.category_list.can_create_category,
+              can_create_topic: result.category_list.can_create_topic,
+            });
+          });
+        },
+      });
+    }
+  },
+};
