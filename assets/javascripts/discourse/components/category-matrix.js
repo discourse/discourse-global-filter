1ca4dac1b6be0230520ca9129032601475bb7f5f
Penar Musaraj <pmusaraj@gmail.com>
2022-11-02 10:42:15 -0400

    UX: Refactor admin UI for managing filter-category mappings (#58)

---
+++ assets/javascripts/discourse/components/category-matrix.js
@@ -0,0 +1,56 @@
+import Component from "@ember/component";
+import discourseComputed from "discourse-common/utils/decorators";
+import BufferedProxy from "ember-buffered-proxy/proxy";
+import { gt, sort } from "@ember/object/computed";
+import { inject as service } from "@ember/service";
+
+export default Component.extend({
+  tagName: "",
+  site: service(),
+  categoriesSorting: ["position"],
+  categoriesOrdered: sort("categoriesBuffered", "categoriesSorting"),
+
+  allCategories: gt("filterTag.categories.length", 0),
+
+  @discourseComputed("site.categories.[]")
+  categoriesBuffered(categories) {
+    const exclUncategorized = categories.filter(
+      (c) =>
+        c.id !== this.site.uncategorized_category_id &&
+        c.parent_category_id !== this.site.uncategorized_category_id
+    );
+
+    return (exclUncategorized || []).map((c) =>
+      BufferedProxy.create({ content: c })
+    );
+  },
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    // order categories by parent (similar to the "reorder categories" modal in core)
+    const reorderChildren = (categoryId, depth, index) => {
+      this.categoriesBuffered.forEach((category) => {
+        if (
+          (categoryId === null && !category.get("parent_category_id")) ||
+          category.get("parent_category_id") === categoryId
+        ) {
+          category.setProperties({ depth, position: index++ });
+          index = reorderChildren(category.get("id"), depth + 1, index);
+        }
+      });
+
+      return index;
+    };
+
+    reorderChildren(null, 0, 0);
+
+    this.categoriesBuffered.forEach((bc) => {
+      if (bc.get("hasBufferedChanges")) {
+        bc.applyBufferedChanges();
+      }
+    });
+
+    this.notifyPropertyChange("categoriesBuffered");
+  },
+});
--- assets/javascripts/discourse/controllers/admin-plugins-filter-tag.js
+++ assets/javascripts/discourse/controllers/admin-plugins-filter-tag.js
@@ -5,40 +5,18 @@ import { popupAjaxError } from "discourse/lib/ajax-error";
 
 export default class extends Controller {
   @action
-  setCategoryIdsForTag(filterTag, categories) {
-    let categoryIds = [];
-    let categoriesAndParents = new Set(categories);
-
-    categories.map((c) => {
-      categoryIds.push(c.id);
-
-      // if category has parent, include parent
-      if (c.parent_category_id) {
-        categoryIds.push(c.parent_category_id);
-        categoriesAndParents.add(c.parentCategory);
-
-        // check if category is subsubcategory
-        if (c.parentCategory.parent_category_id) {
-          categoryIds.push(c.parentCategory.parent_category_id);
-          categoriesAndParents.add(c.parentCategory.parentCategory);
-        }
-      }
-    });
-
-    const data = {
-      category_ids: categoryIds,
-    };
-
+  setCategoryIdsForTag(filterTag, cids) {
     return ajax(
       `/admin/plugins/filter_tags/${filterTag.name}/set_category_ids_for_tag.json`,
       {
-        data,
+        data: {
+          category_ids: cids,
+        },
         type: "POST",
       }
     )
       .then(() => {
-        const uniqCategories = Array.from(categoriesAndParents);
-        filterTag.set("categories", uniqCategories);
+        filterTag.set("category_ids", cids.join("|"));
       })
       .catch(popupAjaxError);
   }
