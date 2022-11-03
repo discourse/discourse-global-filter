ff5702870af740cc312c46ab0b778f5df6850aba
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2022-11-03 11:37:25 -0500

    Refactor apply-global-filter-styles (#62)
    * Refactor apply-global-filter-styles
    * Fix tests

--- assets/javascripts/discourse/initializers/apply-global-filter-styles.js
+++ assets/javascripts/discourse/initializers/apply-global-filter-styles.js
@@ -16,11 +16,11 @@ export default {
     const globalFilters = siteSettings.global_filters.split("|");
     const currentUser = container.lookup("current-user:main");
     const router = container.lookup("router:main");
-    router.on("routeDidChange", () => {
+    router.one("didTransition", () =>
       next(() =>
         this._applyFilterStyles(router, globalFilters, currentUser, container)
-      );
-    });
+      )
+    );
   },
 
   _applyFilterStyles(router, globalFilters, currentUser, container) {
