98230e42fc6ff02c7fc9ca50182ac374bd7ed004
Keegan George <kgeorge13@gmail.com>
2022-11-03 10:33:50 -0700

    DEV: Make `spacedTag` a `@tracked` property for easy override (#64)

--- assets/javascripts/discourse/components/global-filter/filter-item.js
+++ assets/javascripts/discourse/components/global-filter/filter-item.js
@@ -1,11 +1,12 @@
 import Component from "@glimmer/component";
 import { defaultHomepage } from "discourse/lib/utilities";
 import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
 
 export default class GlobalFilterFilterItem extends Component {
   @service site;
 
-  spacedTag = this.args.filter.replace(/-|_/g, " ");
+  @tracked spacedTag = this.args.filter.replace(/-|_/g, " ");
   totalFilterTagCount =
     this.site.filter_tags_total_topic_count[this.args.filter];
 
