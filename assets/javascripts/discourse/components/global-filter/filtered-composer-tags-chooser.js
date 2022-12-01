437cf6b4b86a611bef19408e3b46d0f25946319e
Penar Musaraj <pmusaraj@gmail.com>
2022-12-01 18:41:02 -0500

    DEV: Two small composer refactors
    1. Pass only tags to `ComposerItem`, it's the only property used there.
    2. Use getter for `hiddenValues`, makes it easier to override in theme.

--- assets/javascripts/discourse/components/global-filter/filtered-composer-tags-chooser.js
+++ assets/javascripts/discourse/components/global-filter/filtered-composer-tags-chooser.js
@@ -4,5 +4,7 @@ import { inject as service } from "@ember/service";
 export default class GlobalFilterFilteredComposerTagsChooser extends Component {
   @service siteSettings;
 
-  hiddenValues = this.siteSettings.global_filters.split("|");
+  get hiddenValues() {
+    return this.siteSettings.global_filters.split("|");
+  }
 }
