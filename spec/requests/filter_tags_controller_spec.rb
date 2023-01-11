3af520c6f3e37f051c57823ea71d4976758aec58
Isaac Janzen <isaac.janzen@discourse.org>
2023-01-11 10:33:05 -0600

    update tests

--- spec/requests/filter_tags_controller_spec.rb
+++ spec/requests/filter_tags_controller_spec.rb
@@ -8,20 +8,30 @@ describe GlobalFilter::FilterTagsController do
     let(:tag) { Fabricate(:tag) }
     let(:tag_2) { Fabricate(:tag) }
 
-    it "creates global_filter_preference if one does not exist" do
+    it "creates global_filter_preference if tag is valid global-filter and if preference does not exist" do
+      SiteSetting.global_filters = tag.name
       sign_in(user)
       expect(user.custom_fields).to be_empty
-      put "/global_filter/filter_tags/#{tag.name}/assign.json", params: { user_id: user.id }
+      put "/global_filter/filter_tags/#{tag.name}/assign.json"
       expect(response).to be_successful
       expect(user.reload.custom_fields[:global_filter_preference]).to eq(tag.name)
     end
 
-    it "updates global_filter_preference if it exists" do
+    it "updates global_filter_preference if preference exists" do
+      SiteSetting.global_filters = tag_2.name
       sign_in(user)
       UserCustomField.create(user: user, name: "global_filter_preference", value: tag.name)
-      put "/global_filter/filter_tags/#{tag_2.name}/assign.json", params: { user_id: user.id }
+      put "/global_filter/filter_tags/#{tag_2.name}/assign.json"
       expect(response).to be_successful
       expect(user.reload.custom_fields[:global_filter_preference]).to eq(tag_2.name)
+    end
+
+    it "raises error if tag is not a valid global-filter" do
+      sign_in(user)
+      expect(user.custom_fields).to be_empty
+      put "/global_filter/filter_tags/#{tag.name}/assign.json"
+      expect(response).to_not be_successful
+      expect(user.custom_fields).to be_empty
     end
   end
 end
