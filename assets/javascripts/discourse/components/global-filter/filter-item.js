be81f382e1d8f3f275cde22caecc7aab29900f34
Keegan George <kgeorge13@gmail.com>
2023-01-24 11:32:44 -0800

    UX: Tag topic count should be comma separated

--- assets/javascripts/discourse/components/global-filter/filter-item.js
+++ assets/javascripts/discourse/components/global-filter/filter-item.js
@@ -5,10 +5,17 @@ import { tracked } from "@glimmer/tracking";
 
 export default class GlobalFilterFilterItem extends Component {
   @service site;
+  @service siteSettings;
 
   @tracked spacedTag = this.args.filter.replace(/-|_/g, " ");
-  totalFilterTagCount =
-    this.site.filter_tags_total_topic_count[this.args.filter];
+
+  get totalFilterTagCount() {
+    const count = this.site.filter_tags_total_topic_count[this.args.filter];
+    const defaultLocale =
+      this.siteSettings.default_locale.replace(/_/g, "-") || "en-US";
+
+    return count.toLocaleString(defaultLocale);
+  }
 
   constructor() {
     super(...arguments);
