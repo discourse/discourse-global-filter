53e81b75897407a12d0cca235aae1f4941703293
Isaac Janzen <isaac.janzen@discourse.org>
2023-02-24 09:13:49 -0600

    FEATURE: Add composer dropdown

--- assets/javascripts/discourse/components/global-filter/composer-dropdown.js
+++ assets/javascripts/discourse/components/global-filter/composer-dropdown.js
@@ -1,22 +1,44 @@
 import Component from "@glimmer/component";
-import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
 import { ajax } from "discourse/lib/ajax";
 import { withPluginApi } from "discourse/lib/plugin-api";
 
-export default class GlobalFilterComposerContainer extends Component {
-  @service router;
-  @service site;
-  @service siteSettings;
+export default class GlobalFilterComposerDropdown extends Component {
+  get content() {
+    const filters = this.args.filters;
+    filters.forEach((filter) => {
+      filter.name = this.tagName(filter);
+    });
+    return filters;
+  }
 
-  tagParam = this.router.currentRoute?.queryParams?.tag;
+  get checked() {
+    return this.args.selectedTags?.includes(
+      this.args.filter.name || this.args.tagParam
+    );
+  }
 
-  constructor() {
-    super(...arguments);
+  set checked(value) {
+    return value;
+  }
+
+  tagName(filter) {
+    return filter.alternate_name || filter.name?.replace(/-|_/g, " ") || "";
+  }
+
+  @action
+  toggleTag() {
+    if (this.args.selectedTags.includes(this.args.filter.name)) {
+      const filterIndex = this.args.selectedTags.indexOf(this.args.filter.name);
+      this.args.selectedTags.splice(filterIndex, 1);
+    } else {
+      this.args.selectedTags.push(this.args.filter.name);
+    }
 
     withPluginApi("1.3.0", (api) => {
-      ajax(
-        `/global_filter/filter_tags/categories_for_current_filter.json`
-      ).then((model) => {
+      ajax(`/global_filter/filter_tags/categories_for_filter_tags.json`, {
+        data: { tags: this.args.selectedTags },
+      }).then((model) => {
         api
           .modifySelectKit("category-chooser")
           .replaceContent((categoryDrop) => {
@@ -40,18 +62,5 @@ export default class GlobalFilterComposerContainer extends Component {
           });
       });
     });
-  }
-
-  get canDisplay() {
-    return (
-      (this.args.composer.creatingTopic === true &&
-        !this.args.composer.creatingPrivateMessage) ||
-      (this.args.composer.editingFirstPost === true &&
-        !this.args.composer.privateMessage)
-    );
-  }
-
-  get globalFilters() {
-    return this.site.global_filters;
   }
 }
