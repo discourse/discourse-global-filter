2323194f9b079b6aec24dc3835b8a510de5b8c24
Isaac Janzen <isaac.janzen@discourse.org>
2023-03-09 07:47:36 -0600

    FIX: Update composer category drop with global filter categories

--- assets/javascripts/discourse/components/global-filter/composer-container.js
+++ assets/javascripts/discourse/components/global-filter/composer-container.js
@@ -1,7 +1,5 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
-import { ajax } from "discourse/lib/ajax";
-import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default class GlobalFilterComposerContainer extends Component {
   @service router;
@@ -9,38 +7,6 @@ export default class GlobalFilterComposerContainer extends Component {
   @service siteSettings;
 
   tagParam = this.router.currentRoute?.queryParams?.tag;
-
-  constructor() {
-    super(...arguments);
-
-    withPluginApi("1.3.0", (api) => {
-      ajax(
-        `/global_filter/filter_tags/categories_for_current_filter.json`
-      ).then((model) => {
-        api
-          .modifySelectKit("category-chooser")
-          .replaceContent((categoryDrop) => {
-            if (!categoryDrop.selectKit.filter) {
-              const categoriesAndSubcategories = model.categories.concat(
-                model.subcategories
-              );
-              const filteredSubcategories = categoryDrop.content.filter((c) => {
-                const categoriesByName = categoriesAndSubcategories.map(
-                  (item) => item["name"]
-                );
-
-                return categoriesByName.includes(
-                  c.name ||
-                    categoryDrop.allCategoriesLabel ||
-                    categoryDrop.noCategoriesLabel
-                );
-              });
-              return filteredSubcategories;
-            }
-          });
-      });
-    });
-  }
 
   get canDisplay() {
     return (
