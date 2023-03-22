5d4cefd1ed7aa666ead6bb3cc1246ab0b5d8f3ec
Mark VanLandingham <markvanlan@gmail.com>
2023-03-22 13:42:41 -0500

    frozen_string_literal

--- db/migrate/20230322155754_create_global_filter_topics_by_category_tag.rb
+++ db/migrate/20230322155754_create_global_filter_topics_by_category_tag.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateGlobalFilterTopicsByCategoryTag < ActiveRecord::Migration[7.0]
   def change
     create_table :global_filter_topics_by_category_tags do |t|
