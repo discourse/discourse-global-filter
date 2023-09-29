e897d53f1ba484716e62c456014014e9af176f17
Renato Atilio <renato@discourse.org>
2023-09-29 20:27:03 -0300

    FIX: apply new-topic logic only for logged-in users

--- assets/javascripts/discourse/initializers/global-filter-preference.js
+++ assets/javascripts/discourse/initializers/global-filter-preference.js
@@ -34,7 +34,7 @@ export default {
       let tagFromNewTopic
 
       // on a /new-topic?tags=x route, determine if x is a globalFilter or a child of one
-      if (routeName === "new-topic") {
+      if (routeName === "new-topic" && currentUser) {
         const tags = transition.to?.queryParams?.tags?.split(",");
 
         if (tags) {
