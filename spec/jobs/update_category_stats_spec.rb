fc42cba6ed7ef993ae1f230e26c6d3de97cf7250
Natalie Tay <natalie.tay@gmail.com>
2024-01-04 10:49:23 +0800

    Update spec/jobs/update_category_stats_spec.rb
    Co-authored-by: Alan Guo Xiang Tan <gxtan1990@gmail.com>

--- spec/jobs/update_category_stats_spec.rb
+++ spec/jobs/update_category_stats_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 RSpec.describe GlobalFilter::UpdateCategoryStats do
   describe "#perform" do
     it "does not fail if site setting has non-existent tag name" do
