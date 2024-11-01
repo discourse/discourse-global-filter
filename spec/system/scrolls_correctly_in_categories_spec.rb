d4822af0ed7cac5838f8b7f8c642c27871654b99
Gabriel Grubba <gabriel@discourse.org>
2024-11-01 16:33:50 -0300

    DEV: made test pass

--- spec/system/scrolls_correctly_in_categories_spec.rb
+++ spec/system/scrolls_correctly_in_categories_spec.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
-describe "scrolls correctly in categories", type: :system do
-  fab!(:support_tag) { Fabricate(:tag, name: "support") }
-  fab!(:feature_tag) { Fabricate(:tag, name: "feature") }
+describe "scrolls correctly in categories", type: :system, requires: [:scroll], focus_: true do
   let(:topic_list) { PageObjects::Components::TopicList.new }
 
+  fab!(:support_tag) { Fabricate(:tag, name: "support") }
+  fab!(:feature_tag) { Fabricate(:tag, name: "feature") }
+
+  fab!(:topic) { Fabricate(:topic, tags: [support_tag]) }
+
+  fab!(:first_post) { Fabricate(:post, topic: topic) }
+  fab!(:second_post) { Fabricate(:post, topic: topic, post_number: 2) }
+  fab!(:third_post) { Fabricate(:post, topic: topic, post_number: 3) }
+  fab!(:fourth_post) { Fabricate(:post, topic: topic, post_number: 4) }
+  fab!(:fifth_post) { Fabricate(:post, topic: topic, post_number: 5) }
+
   before do
-    Fabricate.times(21, :topic, tags: [support_tag])
-    Topic.all.each { |t| t.posts << Fabricate(:post, topic: t) }
     SiteSetting.discourse_global_filter_enabled = true
     SiteSetting.global_filters = "support|feature"
     sign_in(Fabricate(:admin))
@@ -20,12 +27,18 @@ describe "scrolls correctly in categories", type: :system do
   it "scrolls correctly when navigating from categories to topic lists, and remembers scroll position when going back" do
     visit "/"
 
-    page.scroll_to(0, 1000)
+    expect(topic_list).to have_topics
     topic_list_scroll_y = current_scroll_y
-    try_until_success { expect(topic_list_scroll_y).to be > 0 }
 
+    try_until_success { expect(topic_list_scroll_y).to eq(0) }
+
+    sleep 1
     topic_list.visit_topic(Topic.first)
-    try_until_success { expect(current_scroll_y).to eq(0) }
+
+    try_until_success do
+      page.scroll_to(find(".powered-by-discourse"))
+      expect(current_scroll_y).to be > 0
+    end
 
     page.go_back
     try_until_success { expect(current_scroll_y).to eq(topic_list_scroll_y) }
